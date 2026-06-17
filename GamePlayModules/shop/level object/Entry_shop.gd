72f6878329e2354375780c7ed00757d3f47257e8
Dragvid <55921056+Dragvid@users.noreply.github.com>
2026-06-17 11:17:22 +0100

    shop implementation

--- GamePlayModules/shop/level object/Entry_shop.gd
+++ GamePlayModules/shop/level object/Entry_shop.gd
@@ -1,10 +1,10 @@
 extends CollisionObject2D
 
 @export var shop_name:String
+@export var return_pos_offset : Vector2
 
 func Load_shop_scene():
-	#AppInfo.Set_position_in_level(position)
-	#AppInfo.enemy_last_battle = get_path()
+	AppInfo.Set_position_in_level(position + return_pos_offset)
 	get_tree().change_scene_to_file(AppInfo.shop_scene)
 
 func _on_area_entered(area: Area2D) -> void:
