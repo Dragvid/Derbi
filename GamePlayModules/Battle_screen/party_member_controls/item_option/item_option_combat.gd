61ba7e8b3ea206aa0e90c6d633299e74bf887d86
Dragvid <55921056+Dragvid@users.noreply.github.com>
2026-06-28 12:35:24 +0100

    item counting fix

--- GamePlayModules/Battle_screen/party_member_controls/item_option/item_option_combat.gd
+++ GamePlayModules/Battle_screen/party_member_controls/item_option/item_option_combat.gd
@@ -8,12 +8,24 @@ extends Button
 
 var play_area_manager
 
+func _ready() -> void:
+	SignalsResource.refresh_item_list.connect(Update_information)
+
 func set_information(new_name,new_item_quant,new_text:String, new_module_manager):
 	item_name_label.text = str(new_name)
 	item_quantity_label.text = str(int(new_item_quant))
 	description_label.text = new_text
 	play_area_manager = new_module_manager
 	#print("play_area_manager: ",play_area_manager.name)
+
+func Update_information():
+	var inventory = AppInfo.save_file_json.item_inventory
+	if inventory.has(item_name_label.text):
+		var item_quant = inventory[item_name_label.text]
+		item_quantity_label.text = str(int(item_quant))
+	else:
+		queue_free()
+		
 
 func _on_focus_entered() -> void:
 	animation_player.play("display_info")
@@ -37,4 +49,5 @@ func _on_button_up() -> void:
 	battle_manager.receive_current_item(play_area_manager, item_info["name"])
 	AppInfo.Remove_item(item_info["name"])
 	#decrement_quantity()
+	SignalsResource.refresh_item_list.emit()
 	battle_manager.toggle_target_selection()
