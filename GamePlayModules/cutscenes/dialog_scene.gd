6ac95007107e6e17bdd9af3be709ba6e7fb77acd
Dragvid <55921056+Dragvid@users.noreply.github.com>
2026-08-11 22:40:50 +0100

    Cenas da história

--- GamePlayModules/cutscenes/dialog_scene.gd
+++ GamePlayModules/cutscenes/dialog_scene.gd
@@ -2,6 +2,8 @@ extends Control
 @export var test_string: String
 @export var time_between_chars: float
 
+@export var episode_to_show : String
+
 @onready var dialog_label: Label = $separator/dialog_box/MarginContainer/MarginContainer/dialog_label
 @onready var speaker_label: Label = $separator/dialog_box/speaker_box/speaker_label
 @onready var portrait_side_a: TextureRect = $separator/portraits/separator/SideA/TextureRect
@@ -15,25 +17,37 @@ var skip_writing: bool = false
 
 func _ready() -> void:
 	#Write_line("test", test_string)
-	Play_scene("test")
+	#Play_scene("test")
+	Play_scene(episode_to_show)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		if is_writing:
 			skip_writing = true  # signal the typewriter to skip
 
+#func Play_scene(scene_name: String):
+	#if not script_json.has(scene_name):
+		#print("Scene not found: ", scene_name)
+		#return
+	#var scene_lines = script_json[scene_name]
+	#for line_number in scene_lines.keys():
+		#var line = scene_lines[line_number]
+		#if line.has("portrait") and line.has("side"):
+			#await update_portrait(line["portrait"], line["side"])
+		#if line.has("speaker") and line.has("text"):
+			#await Write_line(line["speaker"], line["text"])
+		#await wait_for_input()
+
 func Play_scene(scene_name: String):
 	if not script_json.has(scene_name):
 		print("Scene not found: ", scene_name)
 		return
-	var scene_lines = script_json[scene_name]
-	for line_number in scene_lines.keys():
-		var line = scene_lines[line_number]
+	for line in script_json[scene_name]:
 		if line.has("portrait") and line.has("side"):
 			await update_portrait(line["portrait"], line["side"])
 		if line.has("speaker") and line.has("text"):
 			await Write_line(line["speaker"], line["text"])
-		await wait_for_input()
+			await wait_for_input()
 
 func wait_for_input() -> void:
 	await get_tree().create_timer(0.1).timeout
@@ -59,7 +73,11 @@ func update_portrait(new_image_path, side):
 	match side:
 		"a":
 			portrait_side_a.texture = new_image
+			portrait_side_a.modulate.a = 1
+			portrait_side_b.modulate.a = 0.5
 		"b":
 			portrait_side_b.texture = new_image
+			portrait_side_b.modulate.a = 1
+			portrait_side_a.modulate.a = 0.5
 		"background":
 			background_image.texture = new_image
